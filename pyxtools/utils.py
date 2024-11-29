8d5165927d5a15c62926b13b54773aaa1c519b24
ahmed2903 <ahmedmo2903@gmail.com>
2024-11-29 15:59:06 +0100

    added crystal_to_detector_pixels_vector that calculates all the possible outgoing k_vectors

---
+++ pyxtools/utils.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+import pandas
+from math import sin, cos, pi
+
+
+hplanck = 6.62606868E-34  # Js  Plank consant
+c = 299792458  # m/s   Speed of light
+Ang = 1e-10  # m Angstrom
+echarge = 1.6021733E-19  # C  electron charge
+emass = 9.109e-31  # kg Electron rest mass
+r0 = 2.8179403227e-15  # m classical electron radius = e^2/(4pi*e0*me*c^2)
+Cu = 8.048  # Cu-Ka emission energy, keV
+Mo = 17.4808  # Mo-Ka emission energy, keV
+
