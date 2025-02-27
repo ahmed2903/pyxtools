2c9ac0fcfb377aa559db16c0fbc8e68461bee720
ahmed2903 <ahmedmo2903@gmail.com>
2025-02-27 16:15:01 +0100

    epry is now ready for test

--- phase_retrieval/epry_class.py
+++ phase_retrieval/epry_class.py
@@ -4,27 +4,43 @@ from numpy.fft import fftshift, ifftshift, fft2, ifft2
 
 
 from ..utils_pr import *
-from ..data_fs import upsample_images
 
-class EPRyReconstructor:
-    def __init__(self, images, pupil_func, kout_vec, bounds_x, bounds_y, dks, omegas, 
+class EPRy:
+    
+    def __init__(self, images, pupil_func, kout_vec, lr_psize, 
                  num_iter=50, alpha=0.1, beta=0.1, hr_obj_image=None, hr_fourier_image=None):
         self.images = images
         self.pupil_func = pupil_func
         self.kout_vec = kout_vec
-        self.kx_min_n, self.kx_max_n = bounds_x
-        self.ky_min_n, self.ky_max_n = bounds_y
-        self.dkx, self.dky = dks
-        self.omega_obj_x, self.omega_obj_y = omegas
+        self.lr_psize = lr_psize
+        
         self.num_iter = num_iter
         self.alpha = alpha
         self.beta = beta
 
-        if hr_obj_image is None or hr_fourier_image is None:
-            self.hr_obj_image, self.hr_fourier_image = init_hr_image(bounds_x, bounds_y, dks)
-        else:
-            self.hr_obj_image, self.hr_fourier_image = hr_obj_image, hr_fourier_image
+        self.hr_obj_image = hr_obj_image
+        self.hr_fourier_image = hr_fourier_image 
+        
 
+
+    def prepare(self):
+        
+        self.kout_vec = np.array(self.kout_vec)
+        self.images = np.array(self.images)
+        
+        bounds_x, bounds_y, dks = prepare_dims(self.images, self.kout_vec, self.lr_psize)
+        self.kx_min_n, self.kx_max_n = bounds_x
+        self.ky_min_n, self.ky_max_n = bounds_y
+        self.dkx, self.dky = dks
+        
+        omegas = calc_obj_freq_bandwidth(self.lr_psize)
+        self.omega_obj_x, self.omega_obj_y = omegas
+        
+        
+        if self.hr_obj_image is None or self.hr_fourier_image is None:
+            self.hr_obj_image, self.hr_fourier_image = init_hr_image(bounds_x, bounds_y, dks)
+
+        
     def iterate(self):
         nx_lr, ny_lr = self.images[0].shape
         nx_hr, ny_hr = self.hr_obj_image.shape
@@ -37,6 +53,12 @@ class EPRyReconstructor:
 
         self.hr_obj_image = fftshift(ifft2(ifftshift(self.hr_fourier_image)))
         return self.hr_obj_image, self.hr_fourier_image, self.pupil_func
+
+    def compute_weight_fac(func):
+        """Compute weighting factor for phase retrieval update."""
+        
+        mod = np.abs(func) ** 2
+        return np.conjugate(func) / (mod.max() + 1e-23)
 
     def _update_spectrum(self, image, kx_iter, ky_iter, nx_lr, ny_lr):
         """Handles the Fourier domain update."""
@@ -55,8 +77,7 @@ class EPRyReconstructor:
         image_lr_update = np.sqrt(image) * np.exp(1j * np.angle(image_lr))
         image_FT_update = fftshift(fft2(ifftshift(image_lr_update)))
 
-        mod_pupil = np.abs(pupil_func_patch) ** 2
-        weight_fac_pupil = np.conjugate(pupil_func_patch) / (mod_pupil.max() + 1e-23)
+        weight_fac_pupil = self.beta * self.compute_weight_fac(pupil_func_patch)
 
         delta_lowres_ft = image_FT_update - image_FT
-        self.hr_fourier_image[kx_lidx:kx_hidx, ky_lidx:ky_hidx] += delta_lowres_ft * 0.9 * weight_fac_pupil
+        self.hr_fourier_image[kx_lidx:kx_hidx, ky_lidx:ky_hidx] += delta_lowres_ft *  weight_fac_pupil
